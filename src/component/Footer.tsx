7f730359f2afd56e8da28a1b04fc70a6dd63fceb
Raytarra <maxime.waechter@gmail.com>
2026-01-21 17:47:08 +0100

    feat: Début Footer

---
+++ src/component/Footer.tsx
@@ -0,0 +1,21 @@
+import linkedinIcon from "../assets/linkedin.svg"; 
+import youtubeIcon from "../assets/youtube.svg";
+
+export default function Footer() {
+    return (
+        <footer className="flex items-center px-4 bg-white h-32">
+            <div className="flex items-center justify-center gap-4 mt-6 ml-20">
+                <p>Mention légales</p>
+                <p>Politique de cookies</p>
+                <p>Cookies</p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4 mt-10 ml-auto mr-6">
+                <p>Suivez nous sur nos réseaux sociaux</p>
+                <div className="flex items-center justify-center">
+                    <img src={youtubeIcon} alt="Icon Youtube" />
+                    <img src={linkedinIcon} alt="Icon Linkedin" />
+                </div>
+            </div>
+        </footer>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import Footer from "../component/Footer";
 import Header from "../component/Header";
 
 export default function HomePage() {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }; 
