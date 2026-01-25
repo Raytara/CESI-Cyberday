fd552027a3a11ccb2ee5d21060cecb65567cad5e
Raytarra <maxime.waechter@gmail.com>
2026-01-25 18:41:01 +0100

    fix: design footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -3,15 +3,15 @@ import youtubeIcon from "../assets/youtube.svg";
 
 export default function Footer() {
     return (
-        <footer className="flex items-center px-4 bg-white h-32">
+        <footer className="flex items-center px-4 bg-white h-32 font-quicksand font-regular relative z-20">
             <div className="flex items-center justify-center gap-4 mt-6 ml-20">
                 <p>Mention légales</p>
                 <p>Politique de cookies</p>
                 <p>Cookies</p>
             </div>
-            <div className="flex flex-col items-center justify-center gap-4 mt-10 ml-auto mr-6">
+            <div className="flex flex-col items-center justify-center mt-10 ml-auto mr-6">
                 <p>Suivez nous sur nos réseaux sociaux</p>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center mb-4">
                     <img src={youtubeIcon} alt="Icon Youtube" />
                     <img src={linkedinIcon} alt="Icon Linkedin" />
                 </div>
