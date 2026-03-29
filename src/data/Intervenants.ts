e9e3a887017adca50e67cc16fc1999ced759cfd1
Raytarra <maxime.waechter@gmail.com>
2026-03-29 17:07:26 +0200

    fix: clear data

--- src/data/Intervenants.ts
+++ src/data/Intervenants.ts
@@ -7,34 +7,4 @@ export const intervenants = [
     bio: "Spécialiste en cybersécurité et tests d'intrusion, il accompagne les organisations dans l'identification des vulnérabilités et le renforcement de leurs infrastructures.Co‑gérant de NevaSec, il est également formateur en cybersécurité dans plusieurs écoles supérieures.",
     photo: AlexisPhoto
   },
-  {
-    id: 2,
-    name: "Alice Dupont",
-    bio: "Alice est experte en sécurité des systèmes d'information avec plus de 10 ans d'expérience.",
-    photo: "https://randomuser.me/api/portraits/women/1.jpg",
-  }, 
-  {
-    id: 3,
-    name: "Bob Martin",
-    bio: "Bob est un consultant en cybersécurité spécialisé dans la protection des données.",
-    photo: "https://randomuser.me/api/portraits/men/2.jpg",
-  }, 
-  {
-    id: 4,
-    name: "Claire Bernard",
-    bio: "Claire est une analyste en sécurité informatique passionnée par la prévention des cyberattaques.",
-    photo: "https://randomuser.me/api/portraits/women/3.jpg",
-  },
-  {
-    id: 5,
-    name: "Claire Bernard",
-    bio: "Claire est une analyste en sécurité informatique passionnée par la prévention des cyberattaques.",
-    photo: "https://randomuser.me/api/portraits/women/3.jpg",
-  },
-  {
-    id: 6,
-    name: "Claire Bernard",
-    bio: "Claire est une analyste en sécurité informatique passionnée par la prévention des cyberattaques.",
-    photo: "https://randomuser.me/api/portraits/women/3.jpg",
-  },
 ]; 
