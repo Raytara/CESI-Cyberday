3f6d370d52dc38b2748e5beddb91a904d3205ce4
benjamin <benjamin.rapt@viacesi.fr>
2026-04-27 10:30:05 +0200

    Mise à jour des entreprises partenaires

--- src/data/Entreprise.ts
+++ src/data/Entreprise.ts
@@ -1,27 +1,75 @@
-import InfinigateLogo from "../assets/Entreprise/infinigate.jpg"
-import StormshieldLogo from "../assets/Entreprise/stormshield.jpg"
+import CheckPointLogo from "../assets/Entreprise/Checkpoint.svg"
+import FortinetLogo from "../assets/Entreprise/Fortinet.svg"
+import CreditAgricoleLogo from "../assets/Entreprise/CreditAgricole.svg"
+import StormshieldLogo from "../assets/Entreprise/Stormshield.jpg"
 import OrangeLogo from "../assets/Entreprise/OrangeCyber.jpg"
+import TrendMicroLogo from "../assets/Entreprise/TrendMicro.png"
+import SpieLogo from "../assets/Entreprise/Spie.png"
+import BouyguesLogo from "../assets/Entreprise/Bouygues.png"
+import CapgeminiLogo from "../assets/Entreprise/Capgemini.png"
 
 export const entreprises = [
     {
         id: 1,
-        image: InfinigateLogo,
-        name: "Infinigate France",
-        lien: "https://www.infinigate.com/fr/",
-        description: "Infinigate France est une entreprise spécialisée dans la distribution de solutions de cybersécurité. Elle accompagne les partenaires IT dans le choix et le déploiement de technologies visant à sécuriser les systèmes et les données.",
-    }, 
+        image: FortinetLogo,
+        name: "Fortinet",
+        lien: "https://www.fortinet.com/fr/",
+        description: "Fortinet est un leader mondial en matière de solutions de cybersécurité et de pare-feu nouvelle génération. Ses technologies visent à protéger les réseaux, les applications et les données contre un large éventail de menaces numériques, en combinant fonctionnalités de sécurité avancées et performances élevées.",
+    },
     {
-        id: 2, 
+        id: 2,
+        image: CheckPointLogo,
+        name: "CheckPoint",
+        lien: "https://www.checkpoint.com/fr/",
+        description: "Check Point est une entreprise spécialisée dans le développement de solutions de cybersécurité. Elle propose des plateformes et technologies permettant de protéger les systèmes, réseaux et données, et s’appuie sur un réseau de partenaires pour leur déploiement et leur intégration.",
+    },
+    {
+        id: 3,
         image: OrangeLogo,
         name: "Orange Cyberdefense",
         lien: "https://www.orangecyberdefense.com/fr/",
         description: "Orange Cyberdefense est une entité spécialisée dans la cybersécurité, qui accompagne les entreprises dans la protection de leurs systèmes d’information face aux menaces numériques. Elle propose des services de prévention, de détection et de réponse aux incidents de sécurité.",
     },
     {
-        id: 3, 
-        image: StormshieldLogo, 
+        id: 4,
+        image: StormshieldLogo,
         "name": "Stormshield",
-        "lien": "https://www.stormshield.com/fr/", 
+        "lien": "https://www.stormshield.com/fr/",
         "description": "Stormshield est une entreprise spécialisée dans la cybersécurité, qui conçoit des solutions de protection des réseaux, des postes de travail et des données. Elle accompagne les organisations dans la sécurisation de leurs systèmes d’information face aux risques numériques.",
+    },
+    {
+        id: 5,
+        image: TrendMicroLogo,
+        "name": "TrendMicro",
+        "lien": "https://www.trendmicro.com/fr/home.html",
+        "description": "Trend Micro est une entreprise spécialisée dans la cybersécurité, qui conçoit des solutions de protection des réseaux, des postes de travail et des données. Elle accompagne les organisations dans la sécurisation de leurs systèmes d’information face aux risques numériques.",
+    },
+    {
+        id: 6,
+        image: CreditAgricoleLogo,
+        "name": "Crédit Agricole",
+        "lien": "https://www.credit-agricole.fr/",
+        "description": "Le Crédit Agricole est un groupe bancaire français, proposant une large gamme de produits et services financiers, notamment des comptes courants, des crédits, des assurances et des solutions d’épargne. Il est présent en France et à l’international, et s’appuie sur un réseau de caisses régionales et de filiales spécialisées.",
+    },
+    {
+        id: 7,
+        image: SpieLogo,
+        "name": "Spie",
+        "lien": "https://www.spie.com/fr/",
+        "description": "Intervenant sur quatre marchés stratégiques, SPIE accompagne ses clients pour réussir la transition énergétique et une transformation numérique responsable. Elle joue un rôle majeur dans la transformation durable des villes et des territoires, la lutte contre le réchauffement climatique, la numérisation des services et le passage à l’industrie 4.0",
+    },
+    {
+        id: 8,
+        image: BouyguesLogo,
+        "name": "Bouygues Telecom",
+        "lien": "https://www.bouyguestelecom.fr/",
+        "description": "Bouygues Telecom est un opérateur français de télécommunications proposant des offres mobiles, internet et fixe aux particuliers et aux professionnels.",
+    },
+    {
+        id: 9,
+        image: CapgeminiLogo,
+        "name": "Capgemini",
+        "lien": "https://www.capgemini.com/fr-fr/",
+        "description": "Capgemini est une entreprise spécialisée dans le conseil, la technologie et accompagne les entreprises du monde entier dans leur transformation en tirant parti des technologies les plus innovantes",
     },
 ]
