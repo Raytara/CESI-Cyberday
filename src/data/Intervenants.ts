95fccf7590c29b804844af72627fa5655821ad16
kXuT <benji.54000@live.fr>
2026-05-20 09:06:13 +0200

    Suppression espace vide en bas de page

--- src/data/Intervenants.ts
+++ src/data/Intervenants.ts
@@ -7,4 +7,10 @@ export const intervenants = [
     bio: "Spécialiste en cybersécurité et tests d'intrusion, il accompagne les organisations dans l'identification des vulnérabilités et le renforcement de leurs infrastructures. Co‑gérant de NevaSec, il est également formateur en cybersécurité dans plusieurs écoles supérieures.",
     photo: AlexisPhoto
   },
+  {
+    id: 2,
+    name: "Clément Loscot",
+    bio: "Ingénieur développeur - INETUM.",
+    photo: AlexisPhoto
+  }
 ]; 
