19cfc7d9d04879e13d271372f57b15d9ddbc928f
kXuT <benji.54000@live.fr>
2026-05-20 22:35:22 +0200

    Mise à jour des intervenants

--- src/data/Intervenants.ts
+++ src/data/Intervenants.ts
@@ -1,5 +1,15 @@
 import AlexisPhoto from "../assets/Personnes/Alexis.jpg";
 import AvatarHomme from "../assets/Personnes/avatar-homme.jpg";
+import JulesPhoto from "../assets/Personnes/jules.jpeg";
+import NicolasPhoto from "../assets/Personnes/nicolas.jpeg";
+import JulienPhoto from "../assets/Personnes/julien.jpeg";
+import FranckPhoto from "../assets/Personnes/franck.jpeg";
+import JeremiePhoto from "../assets/Personnes/jeremie.jpeg";
+import MaximePhoto from "../assets/Personnes/maxime.jpeg";
+import JulienDrossPhoto from "../assets/Personnes/juliendross.jpeg";
+import BenoitPhoto from "../assets/Personnes/benoit.jpeg";
+import FabricePhoto from "../assets/Personnes/fabrice.jpeg";
+import PierrePhoto from "../assets/Personnes/pierre.webp";
 
 export const intervenants = [
   {
@@ -11,7 +21,91 @@ export const intervenants = [
   {
     id: 2,
     name: "Clément Loscot",
-    bio: "Ingénieur développeur - INETUM.",
+    bio: "Ingénieur développeur - INETUM",
     photo: AvatarHomme
+  },
+  {
+    id: 3,
+    name: "Jules BOZOUKLIAN",
+    bio: "Expert technique cybersécurité industrielle - Eiffage Énergie Systèmes",
+    photo: JulesPhoto
+  },
+  {
+    id: 4,
+    name: "Nicolas SCHEFFER",
+    bio: "Business Development Manager - FORTINET",
+    photo: NicolasPhoto
+  },
+  {
+    id: 5,
+    name: "Stanislas MARCON",
+    bio: "Consultant Cybersécurité confirmé - Orange Cyberdefense",
+    photo: AvatarHomme
+  },
+  {
+    id: 6,
+    name: "Julien GUYON",
+    bio: "RSSI - Eurométropole de METZ",
+    photo: JulienPhoto
+  },
+  {
+    id: 7,
+    name: "Franck POLI",
+    bio: "CISO Group - Crédit Agricole",
+    photo: FranckPhoto
+  },
+  {
+    id: 8,
+    name: "Jeremie ROY",
+    bio: "Référent sûreté et cybersécurité - Gendarmerie",
+    photo: JeremiePhoto
+  },
+  {
+    id: 9,
+    name: "Maxime YOUNES",
+    bio: "Public Sector Account Manager - TrendAI",
+    photo: MaximePhoto
+  },
+  {
+    id: 10,
+    name: "Julien DROSS",
+    bio: "Enterprise Account Manager - TrendAI",
+    photo: JulienDrossPhoto
+  },
+  {
+    id: 11,
+    name: "Benoit BAULER",
+    bio: "Consultant senior en cyber résilience - Capgemini",
+    photo: BenoitPhoto
+  },
+  {
+    id: 12,
+    name: "Bastien REPERANT",
+    bio: "Responsable du Centre d’expertise Résilience - Capgemini",
+    photo: AvatarHomme
+  },
+  {
+    id: 13,
+    name: "Fabrice EINFALT",
+    bio: "Business Development Manager SecOps - Fortinet",
+    photo: FabricePhoto
+  },
+  {
+    id: 14,
+    name: "Loïc AMOROS",
+    bio: "Systems Engineer - Fortinet",
+    photo: AvatarHomme
+  },
+  {
+    id: 15,
+    name: "Damien EYNAUD",
+    bio: "Account Manager - CheckPoint",
+    photo: AvatarHomme
+  },
+  {
+    id: 16,
+    name: "Pierre CUINAT",
+    bio: "Security Engineer - CheckPoint",
+    photo: PierrePhoto
   }
 ]; 
